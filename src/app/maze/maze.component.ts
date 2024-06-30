00f26368f44bd75725e1134ba325b32fffdf0c51
Chris Pyles <cpyles@berkeley.edu>
2024-06-29 22:14:36 -0700

    maze component scaffold

---
+++ src/app/maze/maze.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amaze-maze',
+  standalone: true,
+  imports: [],
+  templateUrl: './maze.component.html',
+  styleUrl: './maze.component.scss'
+})
+export class MazeComponent {
+
+}
