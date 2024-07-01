31c63506ac0c51235ae2492c5769e2ef2c1f0d8b
Chris Pyles <cpyles@berkeley.edu>
2024-06-30 18:13:42 -0700

    implement game state service

--- src/app/game-state.service.ts
+++ src/app/game-state.service.ts
@@ -1,18 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
-import { Maze } from '../lib';
+import { Chooser, Dir, Maze, Node } from '../lib';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameStateService {
+  // This is NOT a secure seed -- but does it matter? I think not.
+  private chooserSeed = makeInsecureSeed();
+  private chooser = new Chooser(this.chooserSeed);
+
   private mazeInternal!: Maze;
 
-  reset(size: number): void {
-    this.mazeInternal = new Maze(size);
+  readonly position = signal<Node>(new Node(0, 0, 0, this.chooser));
+
+  readonly path = signal<Node[]>([]);
+
+  reset(size: number, seed?: number): void {
+    seed = seed ?? makeInsecureSeed();
+    this.chooserSeed = seed;
+    this.chooser = new Chooser(seed);
+    this.mazeInternal = new Maze(size, this.chooser);
+    this.position.set(this.maze.start);
+    this.path.set([this.maze.start]);
+  }
+
+  getShareUrl(): string {
+    return `${window.location.origin}/?seed=${this.chooserSeed}`;
   }
 
   get maze(): Maze {
     return this.mazeInternal;
   }
+
+  move(dir: Dir): void {
+    if (!this.canMove(dir)) {
+      console.log(`invalid movement: ${dir}`);
+      return;
+    }
+    const coords = this.position().getNeighborCoordinates(dir);
+    const newPos = this.maze.getNode(coords);
+    this.path.set([...this.path(), newPos]); // TODO: should this detect when the user moves back along the path
+    this.position.set(newPos);
+  }
+
+  private canMove(dir: Dir): boolean {
+    return !this.position().walls[dir];
+  }
+}
+
+function makeInsecureSeed(): number {
+  return new Date().valueOf();
 }
