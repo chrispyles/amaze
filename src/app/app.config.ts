0ad6eea26d4d0188dc211a1a02e7496cdf8868ce
Chris Pyles <cpyles@berkeley.edu>
2024-06-30 19:39:10 -0700

    base href and deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true })],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: APP_BASE_HREF, useValue: '/amaze' },
+  ],
 };
