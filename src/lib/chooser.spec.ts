6eeddd56742865ed13b6909eb3740a8461428b47
Chris Pyles <cpyles@berkeley.edu>
2024-07-01 20:58:43 -0700

    chooser tests

--- src/lib/chooser.spec.ts
+++ src/lib/chooser.spec.ts
@@ -1,5 +1,22 @@
+import { Chooser } from './chooser';
+
 describe('Chooser', () => {
+  let chooser: Chooser;
+
+  beforeEach(() => {
+    chooser = new Chooser(42);
+  });
+
   describe('choose()', () => {
-    it('should choose a random element from the provided array', () => {});
+    it('should choose a random element from the provided array', () => {
+      const arr = new Array(20).fill(0).map((_, i) => i);
+      const got: number[] = [];
+      for (let i = 0; i < 20; i++) {
+        got.push(chooser.choose(arr));
+      }
+      expect(got).toEqual([
+        5, 5, 15, 0, 14, 4, 12, 7, 19, 13, 3, 5, 17, 13, 18, 0, 2, 12, 16, 4,
+      ]);
+    });
   });
 });
